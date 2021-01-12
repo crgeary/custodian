4d6f13067ff5d0eabc413d6bdc0a484fcb0aa5e5
Christopher Geary <hello@crgeary.com>
2021-01-12 18:17:41 +0000

    Proof of concept

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-// ...
+const custodian = (cb: Function): Function => {
+    return async (event: object, context: object, callback: Function) => {
+        try {
+            const res = await cb(event, context, callback);
+            return {
+                isBase64Encoded: false,
+                statusCode: 200,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(res),
+            };
+        } catch (err) {
+            console.error(err);
+            return {
+                isBase64Encoded: false,
+                statusCode: 500,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(err),
+            };
+        }
+    };
+};
+
+export default custodian;
